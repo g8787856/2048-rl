5272653a3916c5006e6ecd57d6019a7d7401b6b8
Blake Shen <g8787856@gmail.com>
2019-01-02 16:03:01 +0800

    第一版本，可以開始運行遊戲，使用了Q-learning

---
+++ README.md
@@ -0,0 +1,19 @@
+# 2048-rl
+===========
+
+我的AI課程期末專題報告，用強化學習做一個2048遊戲代理人
+現在只能用Q-learning來跑，之後會再補上DQN或者AC算法。
+在終端下指令：`python main.py`就可以開始執行
+
+My AI course final project, a reinforcement learning practice
+
+I choose 2048 game as environment, which based on [2048-python](https://github.com/yangshun/2048-python) by yangshun, and I try to edit it to make it suit for RL env.
+
+For now, I use [Reinforcement-learning-with-tensorflow](https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow) by MorvanZhou as the RL agent, it's only Q-learning now, I will try using DQN or AC algorithm to run it.
+
+You can run `python main.py` to start the game.
+
+## References
+[1] yangshun, [2048-python](https://github.com/yangshun/2048-python)(2013).
+
+[2] MorvanZhou, [Reinforcement-learning-with-tensorflow](https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow)(2018), 
---
+++ logic.py
@@ -0,0 +1,223 @@
+#
+# CS1010FC --- Programming Methodology
+#
+# Mission N Solutions
+#
+# Note that written answers are commented out to allow us to run your
+# code easily while grading your problem set.
+from random import *
+
+#######
+#Task 1a#
+#######
+
+# [Marking Scheme]
+# Points to note:
+# Matrix elements must be equal but not identical
+# 1 mark for creating the correct matrix
+
+def new_game(n):
+    matrix = []
+
+    for i in range(n):
+        matrix.append([0] * n)
+    return matrix
+
+###########
+# Task 1b #
+###########
+
+# [Marking Scheme]
+# Points to note:
+# Must ensure that it is created on a zero entry
+# 1 mark for creating the correct loop
+
+def add_two(mat):
+    a=randint(0,len(mat)-1)
+    b=randint(0,len(mat)-1)
+    while(mat[a][b]!=0):
+        a=randint(0,len(mat)-1)
+        b=randint(0,len(mat)-1)
+    mat[a][b]=2
+    return mat
+
+###########
+# Task 1c #
+###########
+
+# [Marking Scheme]
+# Points to note:
+# Matrix elements must be equal but not identical
+# 0 marks for completely wrong solutions
+# 1 mark for getting only one condition correct
+# 2 marks for getting two of the three conditions
+# 3 marks for correct checking
+
+def game_state(mat):
+    reward = 0
+    over = True
+    total = 0
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            total += mat[i][j]
+            if mat[i][j]==64:
+                reward+=0.1
+            elif mat[i][j]==128:
+                reward+=0.2
+            elif mat[i][j]==256:
+                reward+=0.3
+            elif mat[i][j]==512:
+                reward+=0.4
+            elif mat[i][j]==1024:
+                reward+=0.5
+            elif mat[i][j]==2048:
+                return (10, True)
+    for i in range(len(mat)-1): #intentionally reduced to check the row on the right and below
+        for j in range(len(mat[0])-1): #more elegant to use exceptions but most likely this will be their solution
+            if mat[i][j]==mat[i+1][j] or mat[i][j+1]==mat[i][j]:
+                over = False
+    for i in range(len(mat)): #check for any zero entries
+        for j in range(len(mat[0])):
+            if mat[i][j]==0:
+                over = False
+    for k in range(len(mat)-1): #to check the left/right entries on the last row
+        if mat[len(mat)-1][k]==mat[len(mat)-1][k+1]:
+            over = False
+    for j in range(len(mat)-1): #check up/down entries on last column
+        if mat[j][len(mat)-1]==mat[j+1][len(mat)-1]:
+            over = False
+
+    if (total<=100):
+        reward-=2
+    elif (total<=200):
+        reward-=1
+    elif (total<=300):
+        reward-=0.5
+    elif (total<=400):
+        reward+=0.2
+    elif (total<=500):
+        reward+=0.5
+    else :
+        reward+=0.7
+
+    if over:
+        reward -= 1
+    return (reward, over)
+
+###########
+# Task 2a #
+###########
+
+# [Marking Scheme]
+# Points to note:
+# 0 marks for completely incorrect solutions
+# 1 mark for solutions that show general understanding
+# 2 marks for correct solutions that work for all sizes of matrices
+
+def reverse(mat):
+    new=[]
+    for i in range(len(mat)):
+        new.append([])
+        for j in range(len(mat[0])):
+            new[i].append(mat[i][len(mat[0])-j-1])
+    return new
+
+###########
+# Task 2b #
+###########
+
+# [Marking Scheme]
+# Points to note:
+# 0 marks for completely incorrect solutions
+# 1 mark for solutions that show general understanding
+# 2 marks for correct solutions that work for all sizes of matrices
+
+def transpose(mat):
+    new=[]
+    for i in range(len(mat[0])):
+        new.append([])
+        for j in range(len(mat)):
+            new[i].append(mat[j][i])
+    return new
+
+##########
+# Task 3 #
+##########
+
+# [Marking Scheme]
+# Points to note:
+# The way to do movement is compress -> merge -> compress again
+# Basically if they can solve one side, and use transpose and reverse correctly they should
+# be able to solve the entire thing just by flipping the matrix around
+# No idea how to grade this one at the moment. I have it pegged to 8 (which gives you like,
+# 2 per up/down/left/right?) But if you get one correct likely to get all correct so...
+# Check the down one. Reverse/transpose if ordered wrongly will give you wrong result.
+
+def cover_up(mat):
+    new=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    done=False
+    for i in range(4):
+        count=0
+        for j in range(4):
+            if mat[i][j]!=0:
+                new[i][count]=mat[i][j]
+                if j!=count:
+                    done=True
+                count+=1
+    return (new,done)
+
+def merge(mat):
+    done=False
+    for i in range(4):
+         for j in range(3):
+             if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
+                 mat[i][j]*=2
+                 mat[i][j+1]=0
+                 done=True
+    return (mat,done)
+
+
+def up(game):
+        print("up")
+        # return matrix after shifting up
+        game=transpose(game)
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=transpose(game)
+        return (game,done)
+
+def down(game):
+        print("down")
+        game=reverse(transpose(game))
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=transpose(reverse(game))
+        return (game,done)
+
+def left(game):
+        print("left")
+        # return matrix after shifting left
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        return (game,done)
+
+def right(game):
+        print("right")
+        # return matrix after shifting right
+        game=reverse(game)
+        game,done=cover_up(game)
+        temp=merge(game)
+        game=temp[0]
+        done=done or temp[1]
+        game=cover_up(game)[0]
+        game=reverse(game)
+        return (game,done)
